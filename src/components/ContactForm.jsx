b4078f1d932bb6d00fd9ef19132b3f944a88ae79
Daniel Ruben Rivas Saltos <rubenrivas_17@hotmail.com>
2025-11-26 21:36:34 -0500

    CM 11: Ajustes al fromulario de contacto

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -67,7 +67,7 @@ const ContactForm = () => {
 
         try {
             // Cambiar la URL a tu función de Netlify
-            const response = await axios.post('/.helper/contact', {
+            const response = await axios.post('/.netlify/functions/contact', {
                 ...formData,
                 'turnstile-token': turnstileToken,
                 'Origen': 'Landing Page National North South'
